96d4c187f7868e23f3b3f17cbeecb1d700a0838a
Viet Hua <huaviet999@gmail.com>
2021-05-13 14:24:30 +0700

    no message

--- app/src/main/java/com/thesis/android_challenge_w6/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/thesis/android_challenge_w6/presentation/detail/DetailFragment.kt
@@ -26,23 +26,27 @@ class DetailFragment : Fragment() {
         (activity as MainActivity).appBarLayout?.visibility  = View.GONE
         val data = getDataFromBundle()
         data?.let {
-            tv_movie_name!!.text = data.title
-            tv_overview_content!!.text = data.overview
-            tv_censorship_value!!.text = if(data.adult!!) "Not for audiences under 18 years old"
-                                        else "Available for all audiences"
-            tv_popularity_value!!.text = data.popularity.toString()
-            tv_release_date!!.text = data.releaseDate
-            rating_bar!!.rating = (data.voteAverage!!.toFloat() / 10) * 5
-            tv_average_vote_value!!.text = "(${data.voteAverage}/10)"
-            txtVoteCountValue!!.text = data.voteCount.toString()
-            Glide.with(this.requireContext())
-                .load(NowPlayingListAdapter.URL_IMAGE + data.backdropPath)
-                .into(img_backdrop_movie!!)
-
-            Glide.with(this.requireContext())
-                .load(NowPlayingListAdapter.URL_IMAGE + data.posterPath)
-                .into(img_poster_movie!!)
+            showDetailView(data)
         }
+    }
+
+    private fun showDetailView(data : Movie){
+        tv_movie_name!!.text = data.title
+        tv_overview_content!!.text = data.overview
+        tv_censorship_value!!.text = if(data.adult!!) "Not for audiences under 18 years old"
+        else "Available for all audiences"
+        tv_popularity_value!!.text = data.popularity.toString()
+        tv_release_date!!.text = data.releaseDate
+        rating_bar!!.rating = (data.voteAverage!!.toFloat() / 10) * 5
+        tv_average_vote_value!!.text = "(${data.voteAverage}/10)"
+        txtVoteCountValue!!.text = data.voteCount.toString()
+        Glide.with(this.requireContext())
+            .load(NowPlayingListAdapter.URL_IMAGE + data.backdropPath)
+            .into(img_backdrop_movie!!)
+
+        Glide.with(this.requireContext())
+            .load(NowPlayingListAdapter.URL_IMAGE + data.posterPath)
+            .into(img_poster_movie!!)
     }
 
     private fun getDataFromBundle(): Movie? {
